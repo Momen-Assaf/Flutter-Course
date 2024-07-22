64dcd657744f2ff7aa76a7e875ea36f55d80e5c7
Momen-Assaf <momenssf@gmail.com>
2024-07-23 01:02:25 +0300

    lesson 01, 02

---
+++ Dart_Basics/02_variables.dart
@@ -0,0 +1,8 @@
+void main(){
+  int outPut = 2;// initalization
+  print("output int: $outPut");
+
+  late int lateOutPut;// decleration
+  lateOutPut = 42;// allocation
+  print("late output int: $lateOutPut");
+}
