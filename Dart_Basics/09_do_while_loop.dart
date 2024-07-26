7ed625e3a54ca65c24f3ea9d1fbbd3af4e4ca4d1
Momen-Assaf <momenssf@gmail.com>
2024-07-27 00:52:13 +0300

    do while loop

---
+++ Dart_Basics/09_do_while_loop.dart
@@ -0,0 +1,14 @@
+
+void main(){
+  int number = 1;
+  do{//do once, then check loop
+    print(number);
+    number++;
+  }while(number < 6);
+
+  int number1 = 1;
+  do{
+    print(number1);
+    number1++;
+  }while(number1 < 0);
+}
