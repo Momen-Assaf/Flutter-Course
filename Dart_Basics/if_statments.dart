24cdf7fa11c0d647ddb338a7a7e6a519a6207eca
Momen-Assaf <momenssf@gmail.com>
2024-07-27 00:40:02 +0300

    if statments

---
+++ Dart_Basics/if_statments.dart
@@ -0,0 +1,9 @@
+
+void main(){
+  int age = 16;
+  print("user is $age years old");
+
+  if(age >= 18)print("you can enter movie");
+  else if(age >= 16)print("you can enter the movies with ur parents");
+  else print("you can't enter movie");
+}
