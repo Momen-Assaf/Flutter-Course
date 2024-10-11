6969f1aba7128b87602b7dbd339c0d26999fb994
Momen-Assaf <momenssf@gmail.com>
2024-10-11 11:23:06 +0300

    lesson 18

--- 2_Flutter_Basics/lib/main.dart
+++ 2_Flutter_Basics/lib/main.dart
@@ -22,52 +22,63 @@ class MyApp extends StatelessWidget {
             centerTitle: true,
           ),
           body: Center(
-            child: SingleChildScrollView(
-              // physics: const BouncingScrollPhysics(),
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                      color: Colors.green,
-                      padding: const EdgeInsets.all(20),
-                      margin: const EdgeInsets.only(bottom: 10),
-                      child: const Text("First Column child")),
-                  Container(
-                    height: 200,
-                    width: 200,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.blue,
-                    ),
-                    child: const Center(
-                      child: Text(
-                        "Hello world!",
-                        style: TextStyle(
-                          color: Colors.red,
-                          fontSize: 20,
-                          
-                        ),
+              child: SingleChildScrollView(
+            // physics: const BouncingScrollPhysics(),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                    color: Colors.green,
+                    padding: const EdgeInsets.all(20),
+                    margin: const EdgeInsets.only(bottom: 10),
+                    child: const Text("First Column child")),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.blue,
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "Hello world!",
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 20,
                       ),
                     ),
                   ),
-                  SizedBox(
-                      height: 100,
-                      width: 100,
-                      child:
-                          Image.asset("assets/red.png", fit: BoxFit.fitHeight)),
-                  Container(
-                      color: Colors.amber,
-                      height: size.height * 1.5,
-                      width: 40,
-                      child: const Text("PLACEHOLDER")),
-                  Container(
-                      margin: const EdgeInsets.only(top: 10),
-                      color: Colors.red,
-                      child: const Text("last Column child")),
-                ],
-              ),
-            )),
+                ),
+                SizedBox(
+                    height: 100,
+                    width: 100,
+                    child:
+                        Image.asset("assets/red.png", fit: BoxFit.fitHeight)),
+                Container(
+                    color: Colors.amber,
+                    height: size.height * 1.5,
+                    width: 40,
+                    child: const Text("PLACEHOLDER")),
+                Container(
+                    margin: const EdgeInsets.only(top: 10),
+                    color: Colors.red,
+                    child: const Text("last Column child")),
+                Container(
+                  color: Colors.green,
+                  width: 300,
+                  height: 300,
+                  child: LayoutBuilder(builder: (context, constrains) {
+                    return Container(
+                      color: Colors.blue,
+                      width: constrains.maxWidth * 0.6,   
+                      height: constrains.maxHeight / 2,
+                      margin: const EdgeInsets.only(top: 20),
+                      child: const Center(child: Text("Iam in a builder")),
+                    );
+                  }),
+                )
+              ],
+            ),
+          )),
           floatingActionButton: FloatingActionButton(
               onPressed: () => debugPrint('clicked'),
               child: const Icon(Icons.ac_unit)),
